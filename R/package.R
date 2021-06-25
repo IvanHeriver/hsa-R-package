11d16b5b6686ed61c06a7196889c6d08d438bc61
IvanHeriver <ihach_ihach@hotmail.com>
2021-06-25 12:30:30 +0200

    package documentation

---
+++ R/package.R
@@ -0,0 +1,12 @@
+#' HSA (Hydrological Signature Analysis)
+#' 
+#' This package is a set of function to compute hydrological signatures
+#' from streamflow, precipitation and air temperature time series.
+#' 
+#' @docType package.
+#' @author Ivan Horner
+#' @import dplyr, segmented, RcppRoll, Rcpp
+#' @importFrom Rcpp evalCpp
+#' @useDynLib HSA
+#' @name HSA
+NULL
